96ad486c1eabbd2e7c100c89abfffaded51ba07f
Mathieu <matranc03@gmail.com>
2020-04-21 11:30:58 -0400

    optimization

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from secret_key import *
 
-import time
-import pandas as pd
 import requests as rq
 import json
 
@@ -11,7 +9,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 def load_top_artists(lastfm_username, artist_limit, time_period): #artist load limit = 1-1000   ///   time period = overall, 7day, 1month, 3month, 6month, 12month
-    #tic1 = time.perf_counter()
 
     #declares array
     global top_artists_rawarray
@@ -33,19 +30,6 @@ def load_top_artists(lastfm_username, artist_limit, time_period): #artist load l
         artist_playcount = top_artists_requests_json["topartists"]["artist"][x]["playcount"]
         top_artists_playcount_rawarray.append(artist_playcount)
 
-    #tracks time it takes to process
-    #toc1 = time.perf_counter()
-    #print("\nCompleted in "+str((toc1-tic1))+" seconds\n")
-
-def load_top_artist_pandadb():
-    global artists_dataframe
-    #sets panda series
-    top_artists = pd.Series(top_artists_rawarray)
-    top_artists_playcount = pd.Series(top_artists_playcount_rawarray)
-    #combines into single dataframe
-    artists_dataframe = pd.DataFrame({"Artists":top_artists,"Playcount":top_artists_playcount})
-
-
 #need to edit
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -60,12 +44,9 @@ def home():
 
     return render_template("index.html", **globals())
 
-
 if __name__ == "__main__":
     app.run(debug=True)
 
-#top_artists = pd.Series(["Kanye West", "Kid Cudi", "Travis Scott", "Chance the Rapper", "Pusha T"])
-#top_artists_playcount = pd.Series([3630,1927,428,310,281])
 
 """
 pip install:
