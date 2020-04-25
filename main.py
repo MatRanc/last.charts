28ff6d04d9fd95914c8c0140c880f780b23af574
Mathieu <40959576+MatRanc@users.noreply.github.com>
2020-04-25 03:35:55 -0400

    added info bar for current pie chart
    -also added dummy code for starting website with no cache
    -added another replace function for artists with "\" in their name

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def load_top_artists(lastfm_username, artist_limit, time_period): #artist load l
     #declares array
     global top_artists_rawarray
     global top_artists_playcount_rawarray
+    global top_artists_acceptablerange
     top_artists_rawarray = []
     top_artists_playcount_rawarray = []
 
@@ -28,7 +29,7 @@ def load_top_artists(lastfm_username, artist_limit, time_period): #artist load l
 
     for x in range(top_artists_acceptablerange):
         #adds artist name to array
-        artist_name = (top_artists_requests_json["topartists"]["artist"][x]["name"]).replace("'", " ")
+        artist_name = (top_artists_requests_json["topartists"]["artist"][x]["name"]).replace("'", " ").replace("\"", " ") #whats with artists putting \ and ' in there name???
         top_artists_rawarray.append(artist_name)
 
         #adds artist playcount to arrays
@@ -38,7 +39,18 @@ def load_top_artists(lastfm_username, artist_limit, time_period): #artist load l
 @app.route('/', methods=["GET", "POST"])
 def home():
 
-    load_top_artists("MatRanc", 10, "3month")
+    load_top_artists("MatRanc", 1000, "1month")
+
+    #HAVE TO DEFINE IF NO CACHE (CODE BREAKS AND CAUSES ERROR 500 AS daterange AND OTHER VARIABLES ARE CALLED BEFORE DECLARE. THIS DUMMY CODE ALLOWS IT TO INITIALLY RUN THEN USES THE CACHED CODE)
+    global daterange
+    daterange = ""
+    global artistloadlimit
+    artistloadlimit = 1000
+    global username
+    username = "sev"
+    global daterange_proper
+    daterange_proper = "the past month"
+
 
     if request.method == "POST":
         username = str(request.form["username"])
@@ -47,17 +59,40 @@ def home():
 
         result = load_top_artists(username, artistloadlimit, daterange)
 
-    return render_template("index.html", top_artists_rawarray=json.dumps(top_artists_rawarray), top_artists_playcount_rawarray=json.dumps(top_artists_playcount_rawarray))
+    #converts short form from api into proper english
+    if daterange == "7day":
+        daterange_proper = "over the past 7 days"
+    else: pass
+
+    if daterange == "1month":
+        daterange_proper = "over the past month"
+    else: pass
+
+    if daterange == "3month":
+        daterange_proper = "over the past 3 months"
+    else: pass
+
+    if daterange == "6month":
+        daterange_proper = "over the past 6 months"
+    else: pass
+
+    if daterange == "12month":
+        daterange_proper = "over the past year"
+    else: pass
+
+    if daterange == "overall":
+        daterange_proper = "of all time"
+
+    #for artist count
+    if artistloadlimit == 1000:
+        top_artists_acceptablerange_proper = ""
+    else: top_artists_acceptablerange_proper = top_artists_acceptablerange
+
+    return render_template("index.html", top_artists_rawarray=json.dumps(top_artists_rawarray), top_artists_playcount_rawarray=json.dumps(top_artists_playcount_rawarray), username=username, top_artists_acceptablerange_proper=top_artists_acceptablerange_proper, daterange_proper=daterange_proper)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-'''
-todo:
-all time artists + overall doesnt work: gives "SyntaxError: JSON.parse: expected ',' or ']' after array element at line 1 column 10348 of the JSON data" error.
-have html element above chart saying USERNAME's aritst over timeperiod or whatever
-'''
 
 '''
 required packages?:
